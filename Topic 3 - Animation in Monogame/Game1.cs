f057098e1dc1e80bdc195064d9ce7aed64eee5b9
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-11-11 09:29:48 -0500

    Working

--- Topic 3 - Animation in Monogame/Game1.cs
+++ Topic 3 - Animation in Monogame/Game1.cs
@@ -9,6 +9,15 @@ namespace Topic_3___Animation_in_Monogame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Rectangle window;
+
+        Texture2D greyTribbleTexture;
+        Rectangle greyTribbleRect;
+        Vector2 greyTribbleSpeed;
+
+        Texture2D orangeTribbleTexture;
+        Rectangle orangeTribbleRect;
+        Vector2 orangeTribbleSpeed;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,7 +28,15 @@ namespace Topic_3___Animation_in_Monogame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            greyTribbleRect = new Rectangle(300, 10, 100, 100);
+            greyTribbleSpeed = new Vector2(2, 2);
 
+            orangeTribbleRect = new Rectangle(300, 10, 100, 100);
+
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
@@ -28,6 +45,7 @@ namespace Topic_3___Animation_in_Monogame
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            greyTribbleTexture = Content.Load<Texture2D>("tribbleGrey");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,7 +54,12 @@ namespace Topic_3___Animation_in_Monogame
                 Exit();
 
             // TODO: Add your update logic here
-
+            greyTribbleRect.X += (int)greyTribbleSpeed.X;
+            if (greyTribbleRect.Right > window.Width || greyTribbleRect.Left < 0)
+                greyTribbleSpeed.X *= -1;
+            greyTribbleRect.Y += (int)greyTribbleSpeed.Y;
+            if (greyTribbleRect.Bottom > window.Height || greyTribbleRect.Top < 0)
+                greyTribbleSpeed.Y *= -1;
             base.Update(gameTime);
         }
 
@@ -45,7 +68,11 @@ namespace Topic_3___Animation_in_Monogame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
+            _spriteBatch.Draw(greyTribbleTexture, greyTribbleRect, Color.White);
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
